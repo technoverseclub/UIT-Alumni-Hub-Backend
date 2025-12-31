5b8467553f921e87ae78cce2277571b76b2b5992
technoverseclub <technoverseclub@gmail.com>
2025-12-31 16:20:26 +0530

    Update prisma.js

--- backend/utils/prisma.js
+++ backend/utils/prisma.js
@@ -2,7 +2,8 @@ const { PrismaClient } = require("@prisma/client");
 
 const prisma = new PrismaClient();
 
-console.log("Loaded Prisma models:", Object.keys(prisma));
+
 
 module.exports = prisma;
+
 
