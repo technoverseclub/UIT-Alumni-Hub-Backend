7a6608b1623e4e52382d15cae3b0e253fe04c678
technoverseclub <technoverseclub@gmail.com>
2025-12-31 16:17:46 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/auth", require("./routes/auth.routes"));
+app.use("/auth", require("./src/routes/auth.routes"));
 
 module.exports = app;
+
