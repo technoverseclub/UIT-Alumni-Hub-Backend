20e2132bbb52034b66a97657e16acc46f4cdae8d
Brijesh708961 <rbrijesh427@gmail.com>
2026-02-09 11:58:54 +0530

    fixed utils jwt issue

--- backend/utils/jwt.js
+++ backend/utils/jwt.js
@@ -1,4 +1,11 @@
 const jwt = require("jsonwebtoken");
 
 exports.generateToken = (payload) =>
-  jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "7d" });
+  jwt.sign(
+    {
+      ...payload,
+      role: payload.role?.toUpperCase(), // force uppercase
+    },
+    process.env.JWT_SECRET,
+    { expiresIn: "7d" },
+  );
