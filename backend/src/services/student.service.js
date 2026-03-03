32b52ac2c641a2ee1ae6299756e4b1e497bfcade
Raj Kewat <rajkewat0004@gmail.com>
2026-03-03 19:42:19 +0530

    Route consistency update

--- backend/src/services/student.service.js
+++ backend/src/services/student.service.js
@@ -56,6 +56,9 @@ exports.createProfile = async (userId, data, file) => {
 exports.getMyProfile = async (userId) => {
   return prisma.studentProfile.findUnique({
     where: { userId },
+     include: {
+      user: true,
+    },
   });
 };
 
